4887ce5a887a4fc388d82d012bf256cbef680b81
Ashsenior <ashutoshjunior@gmail.com>
2022-03-18 10:29:08 +0530

    created all the models

--- backend/models.py
+++ backend/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+class University(models.Model):
+    address = models.CharField(max_length=500)
+    name = models.CharField(max_length=200, unique=True)
+    logo = models.FileField(upload_to="logo/")
+
+class Member(AbstractUser):
+    university = models.ForeignKey(University, on_delete=models.CASCADE)
+
+class Project(models.Model):
+    name = models.CharField(max_length=200, unique=True)
+    description = models.TextField(max_length=2000)
+    mentor = models.ForeignKey(Member, on_delete=models.SET_NULL, null=True)
+    resource = models.CharField(max_length=200)
+    members = models.ManyToManyField(Member,related_name="projects")
+
+class Image(models.Model):
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to="images/")
+    
--- frontend/src/components/Home/HomePage.js
+++ frontend/src/components/Home/HomePage.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import './HomePage.css';
+import '../w3.css';
 
 export default function HomePage() {
     return (
-        <div>
-            <h1 className='uwu' >This Webapp is running UwU !</h1>
+        <div className='w3-margin w3-padding' >
+            <h1 className='w3-text-blue' >This Webapp is running UwU !</h1>
             <h2 className='uwu' >Now u can START working ;)</h2>
         </div>
     );
